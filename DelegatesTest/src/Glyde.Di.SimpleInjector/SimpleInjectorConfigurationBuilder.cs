8857b13eb2bc118e3a37012e52daf6bfea5e29d4
George Vella <admin@georgevella.com>
2016-09-25 22:44:18 +0200

    Added implementation for SI Configuration Builder

--- DelegatesTest/src/Glyde.Di.SimpleInjector/SimpleInjectorConfigurationBuilder.cs
+++ DelegatesTest/src/Glyde.Di.SimpleInjector/SimpleInjectorConfigurationBuilder.cs
@@ -1,4 +1,5 @@
-﻿using SimpleInjector;
+﻿using System;
+using SimpleInjector;
 
 namespace Glyde.Di.SimpleInjector
 {
@@ -13,17 +14,22 @@ namespace Glyde.Di.SimpleInjector
 
         public void AddTransientService<TContract, TService>() where TContract : class where TService : class, TContract
         {
-            throw new System.NotImplementedException();
+            _container.Register<TContract, TService>(Lifestyle.Transient);
         }
 
         public void AddSingletonService<TContract, TService>() where TContract : class where TService : class, TContract
         {
-            throw new System.NotImplementedException();
+            _container.Register<TContract, TService>(Lifestyle.Singleton);
         }
 
         public void AddScopedService<TContract, TService>() where TContract : class where TService : class, TContract
         {
-            throw new System.NotImplementedException();
+            _container.Register<TContract, TService>(Lifestyle.Scoped);
+        }
+
+        public void AddScopedService<TContract>(Func<TContract> factoryMethod ) where TContract : class
+        {
+            _container.Register(factoryMethod, Lifestyle.Scoped);
         }
     }
 }
